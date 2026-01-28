0ecd94ba179847864e14613675eb92bbc7a5aa43
mikihiroikura <miki.ut.sikepuri@gmail.com>
2026-01-28 16:33:05 +0100

    create launch file for extracting line segments

--- script/record_linesegments_plevio.sh
+++ script/record_linesegments_plevio.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ROS_LAUNCH_FILE=${1:-linefeatures_davis240c.launch}
+ROS_LAUNCH_FILE=${1:-linefeatures.launch}
 DATA_PATH=${2:-/data/rosbag/ICCV2025/MVSEC/indoor_flying1_data.bag}
 OUTPUT_DIR=${3:-~/data/LineSegments/ICCV2025/rosbag_mvsec_indoor1/c2f-efio/csvs}
 START_TRIAL=${4:-1}
@@ -8,10 +8,10 @@ END_TRIAL=${5:-30}
 PLAY_SPEED=${6:-0.2}
 DURATION=${7:-10}
 START_TIME=${8:-0}
+CONFIG_FILE=${9:-/home/plevio/catkin_ws/src/PL-EVIO/config/mono_davis346/davis346_mvsec.yaml}
 
 for i in $(seq $START_TRIAL $END_TRIAL); do
-    docker exec plevio /bin/bash -c "cd ~/catkin_ws && source devel/setup.bash && roslaunch evio_estimator $ROS_LAUNCH_FILE --screen bag_file:=$DATA_PATH play_speed:=$PLAY_SPEED start_time:=$START_TIME duration:=$DURATION"
+    docker exec plevio /bin/bash -c "cd ~/catkin_ws && source devel/setup.bash && roslaunch evio_estimator $ROS_LAUNCH_FILE bag_file:=$DATA_PATH play_speed:=$PLAY_SPEED start_time:=$START_TIME duration:=$DURATION config_path:=$CONFIG_FILE"
 
     docker cp plevio:/home/plevio/lines.csv "$OUTPUT_DIR/$i.csv"
-
 done
